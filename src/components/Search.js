8ed475e7939b9bc35c690b661fb97d07b04e6c23
cinfusino <c.infusino@csu.fullerton.edu>
2018-11-25 22:45:43 -0800

    fixed mess

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,21 +3,32 @@ import React, { Component } from 'react';
 class Search extends Component {
 
   filterUpdate() {
-    const value = this.searchValue.value;
-    this.props.filterUpdate(value, this.props.searchType);
+    const title = this.searchTitle.value;
+    const genre = this.searchGenre.value;
+    this.props.filterUpdate(title, genre);
   }
 
   render() {
     const { searchType } = this.props;
     return (
-      <form>
-        Search By {searchType}
-        <input
-          type="text"
-          ref={ (value) => {this.searchValue = value}}
-          onChange={this.filterUpdate.bind(this)}
-        />
-      </form>
+      <div>
+        <form>
+          Search By Title
+          <input
+            type="text"
+            ref={ (value) => {this.searchTitle = value}}
+            onChange={this.filterUpdate.bind(this)}
+          />
+        </form>
+        <form>
+          Search By Genre
+          <input
+            type="text"
+            ref={ (value) => {this.searchGenre = value}}
+            onChange={this.filterUpdate.bind(this)}
+          />
+        </form>
+      </div>
     )
   }
 }
